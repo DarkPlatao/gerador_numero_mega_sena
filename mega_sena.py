3cb84bd9d3f0ccfde336461d2b54c07a59dd2c1c
Jonathan Alves <82317859+DarkPlatao@users.noreply.github.com>
2021-12-30 21:46:52 -0300

    Evitar que sejam sorteados números iguais

--- mega_sena.py
+++ mega_sena.py
@@ -2,6 +2,8 @@ import random
 
 mega_sena = []
 for i in range(6):
-    x = random.randrange(1, 60)
+    x = random.randrange(1, 61)
+    while x in mega_sena:
+        x = random.randrange(1, 61)
     mega_sena.append(x)
 print(mega_sena)
